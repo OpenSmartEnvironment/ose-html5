8f43f082f2ad21aa22da5fe1eba72b04721eafab
snaselj <snaselj@earth.snasel.net>
2015-12-29 21:53:05 +0100

    Feature: ORM controls

--- content.js
+++ content.js
@@ -23,11 +23,14 @@ exports.addHead('lib/loading.js', 0.1);
 
 // OSE modules
 exports.addModule('lib/browser');
-exports.addModule('lib/dialog');
+exports.addModule('lib/control/button');
+exports.addModule('lib/control/buttons');
+exports.addModule('lib/control/checkbox');
+exports.addModule('lib/control/onoff');
+exports.addModule('lib/control/slider');
+//exports.addModule('lib/dialog');
 exports.addModule('lib/drag');
 exports.addModule('lib/drawer');
-//  exports.addModule('lib/edit');
-exports.addModule('lib/field');
 exports.addModule('lib/header');
 exports.addModule('lib/index');
 exports.addModule('lib/input');
@@ -44,7 +47,7 @@ exports.addModule('lib/wrap.js');
 
 // Other Node.js modules
 exports.addModule('node_modules/punycode/punycode.js', 'punycode');
-exports.addModule('node_modules/url/url.js', 'url');  // TODO move to ose
+exports.addModule('node_modules/url/url.js', 'url');  // TODO move to "ose" package
 
 // Other js
 //exports.addJs('node_modules/hammerjs/hammer.min.js');
